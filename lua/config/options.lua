453e8a557a3f344c7a2c711ca49c9f4a7a8f6cad
MJ Studio <mym0404@gmail.com>
2025-06-12 13:53:27 +0900

    chore: init

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = false
+opt.ambiwidth = "single"
+opt.autowrite = true
---
+++ lua/plugins/core.lua
@@ -0,0 +1,6 @@
+return {
+  "LazyVim/LazyVim",
+  opts = function()
+    require("tokyonight").load()
+  end,
+}
