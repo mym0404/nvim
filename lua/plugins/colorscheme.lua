ddfc2880be32974634598c08613aa9f1db5b3585
MJ Studio <mym0404@gmail.com>
2025-06-12 23:21:05 +0900

    chore: unconscious commit 🦄

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
   -- { "ellisonleao/gruvbox.nvim" },
   {
+    "cocopon/iceberg.vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("iceberg")
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "iceberg",
     },
   },
 }
