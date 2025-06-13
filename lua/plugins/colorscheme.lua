178025066260d2cb10e5dc4e2f613822d439c0e2
MJ Studio <mym0404@gmail.com>
2025-06-13 11:28:24 +0900

    chore: setup lush

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,6 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- {
-  --   "cocopon/iceberg.vim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme("iceberg")
-  --   end,
-  -- },
   {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false,
-    priority = 1000,
-    -- config = function()
-    --   require("github-theme").setup({
-    --     theme_style = "dark_dimmed",
-    --   })
-    --   -- vim.cmd("colorscheme github_dark_dimmed")
-    -- end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "github_dark_default",
-    },
+    "rktjmp/lush.nvim",
+    { dir = "~/.config/nvim/theme", lazy = true },
   },
 }
