894c7d15230a5d6bc12fb5a7d30debf1e69194ca
MJ Studio <mym0404@gmail.com>
2025-06-13 21:28:43 +0900

    chore: unconscious commit 🦄

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -60,7 +60,7 @@ vim.keymap.set("n", "<leader>gr", function()
 end, { noremap = true, silent = true, desc = "Git discard current file changes" })
 
 vim.keymap.set("x", "<leader>gr", function()
-  local start_line, end_line = utils.getStartAndEndLine()
+  local start_line, end_line = utils.get_start_and_end_lines()
   local filepath = vim.fn.expand("%:p")
 
   if filepath == "" then
@@ -68,49 +68,7 @@ vim.keymap.set("x", "<leader>gr", function()
     return
   end
 
-  -- Get git root and relative path
-  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    print("Not in a git repository")
-    return
-  end
+  utils.discard_changes_in_range(filepath, start_line, end_line)
 
-  local relative_path = vim.fn.fnamemodify(filepath, ":p"):gsub("^" .. git_root .. "/", "")
-
-  -- Get original content from HEAD
-  local original_lines = vim.fn.systemlist(string.format("git show HEAD:%s", vim.fn.shellescape(relative_path)))
-  if vim.v.shell_error ~= 0 then
-    print("Failed to get original content")
-    return
-  end
-
-  -- Get current content
-  local current_lines = vim.fn.getline(1, "$")
-
-  -- Check if there are changes in selected range
-  local has_changes = false
-  for i = start_line, end_line do
-    if current_lines[i] ~= (original_lines[i] or "") then
-      has_changes = true
-      break
-    end
-  end
-
-  if not has_changes then
-    print(string.format("No changes found in lines %d-%d", start_line, end_line))
-    return
-  end
-
-  -- Replace selected lines with original content
-  for i = start_line, end_line do
-    if original_lines[i] then
-      vim.fn.setline(i, original_lines[i])
-    else
-      -- If original file was shorter, delete the line
-      vim.fn.setline(i, "")
-    end
-  end
-
-  print(string.format("Restored lines %d to %d", start_line, end_line))
-  utils.goToNormalMode()
+  utils.go_to_normal_mode()
 end, { noremap = true, silent = true, desc = "Git restore selected lines" })
--- lua/utils/utils.lua
+++ lua/utils/utils.lua
@@ -1,6 +1,6 @@
 M = {}
 
-M.getStartAndEndLine = function()
+M.get_start_and_end_lines = function()
   local start_line = vim.fn.line("v")
   local end_line = vim.fn.line(".")
   if start_line > end_line then
@@ -9,9 +9,56 @@ M.getStartAndEndLine = function()
   return start_line, end_line
 end
 
-M.goToNormalMode = function()
+M.go_to_normal_mode = function()
   local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
   vim.api.nvim_feedkeys(esc, "n", false)
+end
+
+M.discard_changes_in_range = function(filepath, start_line, end_line)
+  -- Get git root and relative path
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    print("Not in a git repository")
+    return
+  end
+
+  local relative_path = vim.fn.fnamemodify(filepath, ":p"):gsub("^" .. git_root .. "/", "")
+
+  -- Get original content from HEAD
+  local original_lines = vim.fn.systemlist(string.format("git show HEAD:%s", vim.fn.shellescape(relative_path)))
+  if vim.v.shell_error ~= 0 then
+    print("Failed to get original content")
+    return
+  end
+
+  -- Get current content
+  local current_lines = vim.fn.getline(1, "$")
+
+  -- Check if there are changes in selected range
+  local has_changes = false
+  for i = start_line, end_line do
+    if current_lines[i] ~= (original_lines[i] or "") then
+      has_changes = true
+      break
+    end
+  end
+
+  if not has_changes then
+    print(string.format("No changes found in lines %d-%d", start_line, end_line))
+    return
+  end
+
+  -- Replace selected lines with original content
+  for i = start_line, end_line do
+    if original_lines[i] then
+      vim.fn.setline(i, original_lines[i])
+    else
+      -- If original file was shorter, delete the line
+      vim.fn.setline(i, "")
+    end
+  end
+
+  print(string.format("Restored lines %d to %d", start_line, end_line))
 end
 
 return M
