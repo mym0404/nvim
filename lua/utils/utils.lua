a33788b5c50b389de1fa801799ca29f156e75270
MJ Studio <mym0404@gmail.com>
2025-06-16 20:28:14 +0900

    chore: unconscious commit 🦄

--- lua/utils/utils.lua
+++ lua/utils/utils.lua
@@ -14,54 +14,6 @@ M.go_to_normal_mode = function()
   vim.api.nvim_feedkeys(esc, "n", false)
 end
 
-M.discard_changes_in_range = function(filepath, start_line, end_line)
-  -- Get git root and relative path
-  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    print("Not in a git repository")
-    return
-  end
-
-  local relative_path = vim.fn.fnamemodify(filepath, ":p"):gsub("^" .. git_root .. "/", "")
-
-  -- Get original content from HEAD
-  local original_lines =
-    vim.fn.systemlist(string.format("git show HEAD:%s", vim.fn.shellescape(relative_path)))
-  if vim.v.shell_error ~= 0 then
-    print("Failed to get original content")
-    return
-  end
-
-  -- Get current content
-  local current_lines = vim.fn.getline(1, "$")
-
-  -- Check if there are changes in selected range
-  local has_changes = false
-  for i = start_line, end_line do
-    if current_lines[i] ~= (original_lines[i] or "") then
-      has_changes = true
-      break
-    end
-  end
-
-  if not has_changes then
-    print(string.format("No changes found in lines %d-%d", start_line, end_line))
-    return
-  end
-
-  -- Replace selected lines with original content
-  for i = start_line, end_line do
-    if original_lines[i] then
-      vim.fn.setline(i, original_lines[i])
-    else
-      -- If original file was shorter, delete the line
-      vim.fn.setline(i, "")
-    end
-  end
-
-  print(string.format("Restored lines %d to %d", start_line, end_line))
-end
-
 M.get_current_filetype = function()
   return vim.bo.filetype
 end
