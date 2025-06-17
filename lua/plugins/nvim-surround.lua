c901abdbfe0d7440b90a0988070f557f468e87c4
MJ Studio <mym0404@gmail.com>
2025-06-17 22:11:51 +0900

    chore: unconscious commit

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -2,6 +2,22 @@ return {
   "kylechui/nvim-surround",
   lazy = true,
   event = "VeryLazy",
+  opts = {
+    keymaps = {
+      insert_line = "<C-g>S",
+      insert = "<C-g>s",
+      normal = "ys",
+      normal_cur = "yss",
+      normal_line = "yS",
+      normal_cur_line = "ySS",
+      visual = "S",
+      visual_line = "S",
+      delete = "ds",
+      change = "cs",
+      change_line = "cS",
+    },
+    move_cursor = "sticky",
+  },
 }
 
---  asdㅛㄴasd
+--  <((asdㅛㄴasd))>
