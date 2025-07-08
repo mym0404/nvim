cb07178e357ad199ffe7efe8a9380310a77c0134
MJ Studio <mym0404@gmail.com>
2025-07-08 09:10:57 +0900

    chore: unconscious commit 🦄

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -81,7 +81,7 @@ return {
       accept = {
         -- experimental auto-brackets support
         auto_brackets = {
-          enabled = true,
+          enabled = false,
         },
       },
       list = {
