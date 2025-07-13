b79569e031a0462a97548aaca1e68cc2e671686d
MJ Studio <mym0404@gmail.com>
2025-07-13 16:39:28 +0900

    chore: unconscious commit 🦄

--- lua/plugins/xcodebuild.lua
+++ lua/plugins/xcodebuild.lua
@@ -6,9 +6,11 @@ return {
     "MunifTanjim/nui.nvim",
     "stevearc/oil.nvim", -- (optional) to manage project files
   },
-  config = function()
-    require("xcodebuild").setup({
-      -- put some options here or leave it empty to use default settings
-    })
-  end,
+  opts = {
+    integrations = {
+      pymobiledevice = {
+        enabled = true,
+      },
+    },
+  },
 }
