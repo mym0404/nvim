ebdb5bce24b92c5c090898796abf07a1ed267b82
MJ Studio <mym0404@gmail.com>
2025-07-17 13:34:48 +0900

    chore: unconscious commit 🦄

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -22,7 +22,7 @@ return {
     suggestion = {
       enabled = not vim.g.ai_cmp,
       auto_trigger = true,
-      debounce = 1000,
+      debounce = 300,
 
       hide_during_completion = false,
 
