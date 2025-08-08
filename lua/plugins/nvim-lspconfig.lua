c28ddc19667c89451a33d1349306da926ec33cea
MJ Studio <mym0404@gmail.com>
2025-08-08 23:21:38 +0900

    chore: unconscious commit 🐋

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -79,15 +79,15 @@ return {
       },
     },
     setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
+      -- eslint = function()
+      --   require("lazyvim.util").lsp.on_attach(function(client)
+      --     if client.name == "eslint" then
+      --       client.server_capabilities.documentFormattingProvider = true
+      --     elseif client.name == "tsserver" then
+      --       client.server_capabilities.documentFormattingProvider = false
+      --     end
+      --   end)
+      -- end,
       ["*"] = function(server, config)
         local lspconfig = require("lspconfig")
         local capabilities = config.capabilities
