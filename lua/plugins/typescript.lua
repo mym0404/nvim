1461c1cd1805b9b448dbe471215f0605dfb2384a
MJ Studio <mym0404@gmail.com>
2025-08-10 01:12:52 +0900

    chore: unconscious commit 🐋

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -11,7 +11,7 @@ return {
         -- spawn additional tsserver instance to calculate diagnostics on it
         separate_diagnostic_server = true,
         -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-        publish_diagnostic_on = "insert_leave",
+        publish_diagnostic_on = "change",
         -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
         -- "remove_unused_imports"|"organize_imports") -- or string "all"
         -- to include all supported code actions
@@ -37,7 +37,7 @@ return {
         },
         -- locale of all tsserver messages, supported locales you can find here:
         -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
-        tsserver_locale = "en",
+        tsserver_locale = "ko",
         -- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
         complete_function_calls = false,
         include_completions_with_insert_text = true,
