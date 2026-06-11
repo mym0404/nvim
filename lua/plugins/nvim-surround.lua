bfa98d01814c4b8075fd818ab774276672c81acb
MJ Studio <mym0404@gmail.com>
2026-06-11 09:48:32 +0900

    fix: use default nvim-surround keymaps

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,19 +3,6 @@ return {
   lazy = true,
   event = "VeryLazy",
   opts = {
-    keymaps = {
-      insert_line = "<C-g>S",
-      insert = "<C-g>s",
-      normal = "t",
-      normal_cur = "ts",
-      normal_line = "tS",
-      normal_cur_line = "tS",
-      visual = "t",
-      -- visual_line = "t",
-      delete = "ds",
-      change = "cs",
-      change_line = "cS",
-    },
     move_cursor = "sticky",
   },
 }
